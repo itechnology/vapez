dfcdfaf7a80cf0f25438eb25ed63c3b85e196512
robert-hoffmann <mail.someone@gmail.com>
2014-01-13 17:50:25 +0100

    initial commit

---
+++ source/tools/coil/filters.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+/* Filters */
+
+angular.module('phonecatFilters', [])
+    .filter('checkmark', function () {
+        return function (input) {
+            return input ? '\u2713' : '\u2718';
+        };
+    });
