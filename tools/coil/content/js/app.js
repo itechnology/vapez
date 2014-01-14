ce25534040e864f22e86e490bcbf3c8d496bf6ee
robert-hoffmann <mail.someone@gmail.com>
2014-01-14 01:16:22 +0100

    changes

--- tools/coil/content/js/app.js
+++ tools/coil/content/js/app.js
@@ -6,29 +6,35 @@ var coilApp = angular.module("coil", []);
 coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
 
     // Adjustable
-    $scope.diameter = "1.0";
-    $scope.spires   = "1.0";
-    $scope.feet     = "1.0";
-    $scope.tension  = "3.2";
+    $scope.diameter = "2.0";
+    $scope.spires   = "4.0";
+    $scope.feet     = "5.0";
+    $scope.volts    = "3.2";
 
     // init
     $http.get('content/js/wire.json')
         .success(function (data) {
             $scope.data = data;
+            $scope.selected = 0;
+
             $scope.calculate();
         });
 
     //#region functions
     $scope.up = function (target, value) {
-        $scope[target] = parseFloat($scope[target]);
-        $scope[target] = ($scope[target] += value).toFixed(1);
+        var prev = parseFloat($scope[target]);
+        $scope[target] = (prev += value).toFixed(1);
 
         $scope.calculate();
     }
 
     $scope.down = function (target, value) {
-        $scope[target] = parseFloat($scope[target]);
-        $scope[target] = ($scope[target] -= value).toFixed(1);
+        var prev = parseFloat($scope[target]);
+        var next = (prev -= value);
+
+        if (!!next) {
+            $scope[target] = next.toFixed(1);
+        }
 
         $scope.calculate();
     }
@@ -38,16 +44,14 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
     }
 
     // Results
-    $scope.resistance = 1.0;
-    $scope.watts      = 1.0;
-    $scope.amperes    = 1.0;
-    $scope.coeff      = 1.0;
-    $scope.length     = 1.0;
+    $scope.ohms    = 1.0;
+    $scope.watts   = 1.0;
+    $scope.amperes = 1.0;
+    $scope.length  = 1.0;
 
     $scope.calculate = function () {
         var wire = $scope.data[$scope.selected];
         var dist = ((parseFloat($scope.diameter) * Math.PI) * parseFloat($scope.spires)) + (parseFloat($scope.feet) * 2);
-
 
         // Determine resistance of material
         // http://www.kanthal.com/en/products/materials-in-wire-and-strip-form/wire/resistance-heating-wire-and-resistance-wire/list-of-alloys/
@@ -58,11 +62,10 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
         // Surface mm² = (PI() * ((diameter/2)^2))
         // Surface mm² = (h * w)
 
-
-        $scope.resistance = (dist * r).toFixed(2);
-
-        $scope.length = dist.toFixed(2);
-        //Math.PI;
+        $scope.ohms    = (dist * r).toFixed(2);
+        $scope.watts   = (($scope.volts * $scope.volts) / $scope.ohms).toFixed(2);
+        $scope.amperes = ($scope.volts / $scope.ohms).toFixed(2);
+        $scope.length  = (dist / 10).toFixed(2);
     }
     //#endregion
 });
