fb0e80c2134c269835bfb13c7858e4b214aa3b5c
robert-hoffmann <mail.someone@gmail.com>
2014-01-15 18:02:23 +0100

    more parameters

--- tools/coil/content/js/app.js
+++ tools/coil/content/js/app.js
@@ -5,25 +5,25 @@ var coilApp = angular.module("coil", []);
 
 coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
     // init default adjustable values
-    $scope.diameter = 2.0;
+    $scope.diameter = 2.0;    
     $scope.spires   = 4.0;
     $scope.feet     = 3.0;
     $scope.volts    = 3.7;
+
+    $scope.coils = 1;
+    $scope.wires = 1;
 
     // init
     $http.get("content/js/wire.json")
         .success(function (data) {
             $scope.data = data;
-            $scope.wire = data[1];
+            $scope.wire = data[0];
+            
+            $scope.height = $scope.wire.height;
+            $scope.width  = $scope.wire.width;
 
             $scope.calculate();
         });
-
-    // init default results (not needed ...?)
-    $scope.ohms    = 1.0;
-    $scope.watts   = 1.0;
-    $scope.amperes = 1.0;
-    $scope.length  = 1.0;
     
     //#region functions
     $scope.up = function(target, value) {
@@ -43,24 +43,31 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
     };
 
     $scope.changed = function () {
-        console.log($scope.wire);
+        // We changed wire type, so reset default values
+        $scope.height = $scope.wire.height;
+        $scope.width  = $scope.wire.width;
+        $scope.wires  = 1;
+        
         $scope.calculate();
     };
 
     $scope.calculate = function () {
         // http://jsfiddle.net/he25P/17/
 
-        //#region Surface Area
-        // -> Round (diameter in mm)
-        // var section = Math.PI * (Math.pow(wire.diameter/2, 2);
-
-        // -> Flat (height/width in mm)
-        // var section = (wire.height * wire.width);
-        //#endregion
+        // Surface Area
+        var section = 0;
+        
+        if (!!$scope.wire.flat) {
+            // -> Flat (height/width in mm)
+            section = ($scope.height * $scope.width);
+        } else {
+            // -> Round (diameter in mm)
+            section = Math.PI * (Math.pow($scope.width / 2, 2));
+        }
 
         // Resistance/mm (resistivity in ohm/mm²/meter)
         // http://www.kanthal.com/en/products/materials-in-wire-and-strip-form/wire/resistance-heating-wire-and-resistance-wire/list-of-alloys/
-        var resistance = ($scope.wire.resistivity / $scope.wire.section) / 1000;
+        var resistance = ($scope.wire.resistivity / (section * $scope.wires)) / 1000;
 
         // Helicoidal length (1 turn)
         // http://fr.wikipedia.org/wiki/H%C3%A9lice_%28g%C3%A9om%C3%A9trie%29
@@ -73,17 +80,21 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
         //
         // C  = pi x D
         // Lc = sqrt(C^2 + P^2)
-        // Lt = N x Lc 
-        var length = Math.sqrt(Math.pow(Math.PI * $scope.diameter, 2) + Math.pow($scope.wire.width, 2));
+        // Lt = N x Lc
+        var width = $scope.wire.width;
+        if ($scope.wires > 1) {
+            width = (width * 3);
+        }
+        var length = Math.sqrt(Math.pow(Math.PI * $scope.diameter, 2) + Math.pow(width, 2));
 
         // Total distance (coil + feet)
         var dist = (length * $scope.spires) + ($scope.feet * 2);
 
         // Display
-        $scope.ohms    = dist * resistance;
+        $scope.ohms    = dist * (resistance/$scope.coils);
         $scope.watts   = Math.pow($scope.volts, 2) / $scope.ohms;
         $scope.amperes = $scope.volts / $scope.ohms;
-        $scope.length = dist / 10;
+        $scope.length  = dist / 10;
 
         // Visual effects
         $scope.warningAmperes = ($scope.amperes > 5.5);
