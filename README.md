0098278b00893c63e141d68314506327ac123475
robert-hoffmann <mail.someone@gmail.com>
2014-01-17 00:04:34 +0100

    coeff de chauffe, sauvegarde

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-vapez
-=====
+VaPeZ
+===
 
 Blog & outils collaboratifs autour de la vap.
 
+* http://vapez.fr
+* http://facebook.com/vapez
+
+
+Le but est de creer un site collaboratif autour de la vap (outils, bons plans, infos, tutoriaux, reviews, etc).
+
+Un peu de redaction, et un peu de recueillage d'informations/liens.
+
+Si ca vous dit d'aider/participer, contactez moi (bobby31) sur le forum: http://forum-ecigarette.com 
+
+Merci !
--- tools/coil/content/js/app.js
+++ tools/coil/content/js/app.js
@@ -4,39 +4,71 @@
 var coilApp = angular.module("coil", []);
 
 coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
-    // init default adjustable values
-    $scope.diameter = 2.0;    
-    $scope.spires   = 4.0;
-    $scope.feet     = 3.0;
-    $scope.volts    = 3.7;
+    $scope.close = function() {    
+        $scope.dialog = false;
+    };
+    $scope.open = function (text) {
+        $scope.text   = text;
+        $scope.dialog = true;
+    };
+    
+    // default model
+    $scope.model = {
+        wire: {
+            category   : "Kanthal A1",
+            resistivity: 1.45,
+            height     : 0.20,
+            width      : 0.20,
+            flat       : false,
+            count      : 1
+        },
+        coil: {
+            diameter: 1.8,
+            turns   : 5,
+            stubs   : 4,
+            count   : 1,
+            volts   : 3.7
+        }        
+    };
 
-    $scope.coils = 1;
-    $scope.wires = 1;
-
+    $scope.result = {
+        // placeholder for results
+    };
+    
     // init
     $http.get("content/js/wire.json")
         .success(function (data) {
             $scope.data = data;
-            $scope.wire = data[0];
-            
-            $scope.height = $scope.wire.height;
-            $scope.width  = $scope.wire.width;
 
+            load();
+            
+            if (!$scope.LOADED) {
+                $scope.model.wire = data[0];                
+            } else {
+                for (var i = 0, l = data.length; i < l; i++) {
+                    if ($scope.model.wire.category === data[i].category) {
+                        $scope.model.wire = data[i];
+                    }
+                }
+            }
+            
+            // add missing value
+            $scope.model.wire.count = 1;            
             $scope.calculate();
         });
     
     //#region functions
-    $scope.up = function(target, value) {
-        $scope[target] += value;
+    $scope.up = function (model, attribute, step) {
+        $scope.model[model][attribute] += step;
 
         $scope.calculate();
     };
 
-    $scope.down = function(target, value) {
-        var next = ($scope[target] - value);
+    $scope.down = function(model, attribute, step) {
+        var next = ($scope.model[model][attribute] - step);
 
-        if (!!next) {
-            $scope[target] = next;
+        if (next > 0) {
+            $scope.model[model][attribute] = next;
         }
 
         $scope.calculate();
@@ -44,33 +76,38 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
 
     $scope.changed = function () {
         // We changed wire type, so reset default values
-        $scope.height = $scope.wire.height;
-        $scope.width  = $scope.wire.width;
-        $scope.wires  = 1;
+        $scope.model.wire.count = 1;
         
         $scope.calculate();
     };
-
+    
     $scope.calculate = function () {
         // http://jsfiddle.net/he25P/17/
+        
+        var model  = $scope.model;
+        var result = $scope.result;
+        
+        if (model.coil.volts > 8.4) {
+            model.coil.volts = 8.4;
+        }
 
         // Surface Area
         var section = 0;
         
-        if (!!$scope.wire.flat) {
+        if (!!model.wire.flat) {
             // -> Flat (height/width in mm)
-            section = ($scope.height * $scope.width);
+            section = (model.wire.height * model.wire.width);
         } else {
             // -> Round (diameter in mm)
-            section = Math.PI * (Math.pow($scope.width / 2, 2));
+            section = Math.PI * (Math.pow(model.wire.width / 2, 2));
             
             // since working with round wire
-            $scope.height = $scope.width;
+            model.wire.height = model.wire.width;
         }
 
         // Resistance/mm (resistivity in ohm/mm²/meter)
         // http://www.kanthal.com/en/products/materials-in-wire-and-strip-form/wire/resistance-heating-wire-and-resistance-wire/list-of-alloys/
-        var resistance = ($scope.wire.resistivity / (section * $scope.wires)) / 1000;
+        var resistance = (model.wire.resistivity / (section * model.wire.count)) / 1000;
 
         // Helicoidal length (1 turn)
         // http://fr.wikipedia.org/wiki/H%C3%A9lice_%28g%C3%A9om%C3%A9trie%29
@@ -84,30 +121,54 @@ coilApp.controller("CoilCtrl", function ($scope, $http, $filter) {
         // C  = pi x D
         // Lc = sqrt(C^2 + P^2)
         // Lt = N x Lc
-        var width  = $scope.width  * $scope.wires;
-        var height = $scope.height * $scope.wires;
-        var length = Math.sqrt(Math.pow(Math.PI * ($scope.diameter + height), 2) + Math.pow(width, 2));
+        var height = model.wire.height;
+        var width  = model.wire.width * model.wire.count;
+        var length = Math.sqrt(Math.pow(Math.PI * (model.coil.diameter + height), 2) + Math.pow(width, 2));
 
         // Total distance (coil + feet)
-        var dist = (length * $scope.spires) + ($scope.feet * 2) + $scope.diameter;
+        var dist = (length * model.coil.turns) + (model.coil.stubs * 2) + model.coil.diameter;
 
         // Display
-        $scope.ohms    = dist * (resistance/$scope.coils);
-        $scope.watts   = Math.pow($scope.volts, 2) / $scope.ohms;
-        $scope.amperes = $scope.volts / $scope.ohms;
-        $scope.length  = dist / 10;
-
+        result.ohms   = dist * (resistance / model.coil.count);
+        result.watts  = Math.pow(model.coil.volts, 2) / result.ohms;
+        result.amps   = model.coil.volts / result.ohms;
+        result.length = dist / 10;
+        result.width  = (model.coil.turns * width) / 10;
+        
+        // Thank's to luc.bigjohn !
+        // https://docs.google.com/file/d/0BxGcL1JOoEcySXpUTklhZGNfY28/edit
+        if (model.wire.flat) {
+            result.coeff = (result.watts / model.coil.count) / (2 * (model.wire.height + model.wire.width) * dist);
+        } else {
+            result.coeff = (result.watts / model.coil.count) / ((Math.PI * model.wire.width) * dist);            
+        }
+        
         // Visual effects
-        $scope.warningAmperes = ($scope.amperes > 5.5);
+        result.cssOhms  = (result.ohms < 0.5);
+        result.cssAmps  = (result.amps > 5.5);
+        result.cssWatts = (result.watts > 15);
+        result.cssCoeff = (result.coeff >= 0.2 && result.coeff <= 0.3);
 
-        //save();
+       save();
     };
 
     function load() {
         // load from localStorage
+        var model = localStorage.getItem("settings");
+        
+        if (!!model) {
+            model = JSON.parse(model);
+
+            if (angular.isObject(model)) {
+                $scope.model  = model;
+                $scope.LOADED = true;               
+            }            
+        }
     }
+    
     function save() {
         // save to localStorage
+        localStorage.setItem("settings", JSON.stringify($scope.model));
     }
     //#endregion
 });
